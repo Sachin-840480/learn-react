64e128ee26e40d6d62575a5d5831c8a56d2180e3
Sachin-840480 <sachin1712003@gmail.com>
2026-07-13 16:20:59 +0530

    new simple state learned

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Sachin'
+      name: 'Sachin',
+      company: 'ztm'
     }
   }
 
@@ -19,10 +20,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name}, I work at {this.state.company}
           </p>
           <button onClick={() => {
-            this.setState({name : 'Satyam'})
+            this.setState({name : 'Satyam'});
+            console.log(this.state);
           }}>
             Change Name
           </button>
