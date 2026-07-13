f2bec22449b1cbb1f0d3b694b2b5b96615c10782
Sachin-840480 <sachin1712003@gmail.com>
2026-07-13 16:41:30 +0530

    set state and secondary callback in return function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,14 +4,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      name: {firstname: 'Sachin', lastname: 'Kumar'},
-      company: 'ztm'
-    }
+      name: { firstname: "Sachin", lastname: "Kumar" },
+      company: "ztm",
+    };
   }
 
   render() {
@@ -20,12 +19,23 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name.firstname} {this.state.name.lastname}, I work at {this.state.company}
+            Hi {this.state.name.firstname} {this.state.name.lastname}, I work at{" "}
+            {this.state.company}
           </p>
-          <button onClick={() => {
-            this.setState({name : {firstname: 'Satyam', lastname: 'Kumar'}});
-            console.log(this.state);
-          }}>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstname: "Satyam", lastname: "Kumar" },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                },
+              );
+            }}
+          >
             Change Name
           </button>
         </header>
